a9581dbf33aaa72ba1b72928358fa5e9a2ebdf7d
passfinder <shotawebcoding@gmail.com>
2025-05-22 22:19:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -17,34 +17,43 @@ BULLET_SPEED = 4
 # プレイヤークラス
 # ----------------------------
 class Player:
-    def __init__(self, x, y): # 初期化処理
-        self.x = x # プレイヤーのx座標
-        self.y = y # プレイヤーのy座標
-        self.bullet = None # 弾の状態
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.bullet = None
+        self.direction = "neutral"  # "left", "right", "neutral"
 
     def update(self):
-        speed = 2 # プレイヤーの速度(2 pixel/1 frame)
+        speed = 2
+        self.direction = "neutral"  # 毎フレーム初期化
 
-        if pyxel.btn(pyxel.KEY_LEFT): # 左キーを押下した場合
-            self.x = max(0, self.x - speed) 
-            # max(a, b) は「a と b のうち、大きい方を返す」
-            # 0 より小さくなりそうなら 0 に止める
+        if pyxel.btn(pyxel.KEY_LEFT):
+            self.x = max(0, self.x - speed)
+            self.direction = "left"
 
-        if pyxel.btn(pyxel.KEY_RIGHT): # 右キーを押下した場合
+        if pyxel.btn(pyxel.KEY_RIGHT):
             self.x = min(SCREEN_WIDTH - PLAYER_WIDTH, self.x + speed)
+            self.direction = "right"
 
         if pyxel.btnp(pyxel.KEY_SPACE) and self.bullet is None:
             self.bullet = [self.x + PLAYER_WIDTH // 2, self.y]
             pyxel.play(0, 0)
 
-        if self.bullet is not None: # 弾が発射されているかどうか判定
+        if self.bullet is not None:
             self.bullet[1] -= BULLET_SPEED
             if self.bullet[1] + BULLET_HEIGHT < 0:
                 self.bullet = None
 
     def draw(self):
-        # pyxel.blt(x, y, img, u, v, w, h, colkey)
-        pyxel.blt(self.x, self.y, 0, 0, 0, PLAYER_WIDTH, PLAYER_HEIGHT, 0)
+        if self.direction == "right":
+            pyxel.blt(self.x, self.y, 0, 0, 16, 16, 16, 0)
+
+        if self.direction == "left":
+            pyxel.blt(self.x, self.y, 0, 0, 32, 16, 16, 0)
+
+        if self.direction == "neutral":
+            pyxel.blt(self.x, self.y, 0, 0, 0, PLAYER_WIDTH, PLAYER_HEIGHT, 0)
+
         if self.bullet is not None:
             pyxel.rect(self.bullet[0], self.bullet[1], BULLET_WIDTH, BULLET_HEIGHT, 7)
 
